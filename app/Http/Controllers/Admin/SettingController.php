ec2cbbb638080b29b742d004f4c6c9c42ce8c9a2
Amauri de Souza <amauridesouzaferreira@gmail.com>
2020-03-26 11:13:03 -0300

    Criado preenchimentos dos campos da tela de configurações

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -4,14 +4,28 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Setting;
 
 class SettingController extends Controller
 {   
+    public function __construct(){
+        $this->middleware('auth');
+    }
     /**
      * crio meu método que exibe minha tela de configurações
      */
     public function index(){
 
-        return view('admin.settings.index');
+        $settings = [];
+
+        $dbsettings = Setting::get();
+
+        foreach($dbsettings as $dbsetting) {
+            $settings[ $dbsetting['name']] = $dbsetting['content']; 
+        }
+
+        return view('admin.settings.index', [
+            'settings' => $settings
+        ]);
     }
 }
